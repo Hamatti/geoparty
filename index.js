05814ec0ec38ebab38ea7ee1425408c5f341d7e8
Juha-Matti Santala <juhamattisantala@gmail.com>
2014-10-16 18:53:30 -0400

    Progress

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
-var User = require('./player.js').user;
-var questions = require('./questions.json');
+var util = require('./player.js');
+var User = util.user;
+var Show = util.show;
+var questions = require('./737.json');
 var path = require('path');
 var http = require('http').Server(app);
-
+var _ = require('underscore-node');
 var io = require('socket.io')(http);
+
+
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/img', express.static(path.join(__dirname, '/img')));
 app.use('/js', express.static(path.join(__dirname, '/js')));
@@ -21,25 +25,27 @@ app.get('/game/', function(req, res) {
 
 var players = [];
 var i = 0;
+var gameStarted = false;
+var round = 0;
 
+var show = new Show(questions);
 io.on('connection', function(socket) {
     if(players.length >= 3) {
         console.log('Game is full');
-        socket.emit('error', { msg: 'This game is full' });
         return;
     }
     else {
         var player = new User('player' + i++);
-        console.log(player);
         players.push(player)
-    
         console.log(player.name + ' joined the game');
         console.log(players);
         io.emit('playerChange', { 'players': players });
 
-        if(players.length == 3) {
+        if(true || (players.length == 3 && !gameStarted)) {
             // Start the game
-            io.emit('gameInit', {});
+            gameStarted = true;
+            playQuestions = show.getQuestions(0);
+            io.emit('gameInit', {round: 1, questions: playQuestions});
         }
         socket.on('disconnect', function() {
             console.log('user ' + player.name + ' disconnected');
@@ -52,8 +58,21 @@ io.on('connection', function(socket) {
         console.log("Changed name: " + player.name);
         io.emit('playerChange', { 'players': players });
     });
-});
 
+    socket.on('getQuestion', function(key) {
+        var question = show.getQuestion(key);
+        io.emit('showQuestion', question);
+    });
+
+    socket.on('guess', function(data) {
+        var question = show.getQuestion(data.question);
+        var correctAnswer = question.answer;
+        io.emit('rightAnswer', {points: 0, answer: correctAnswer});
+        console.log(question.question);
+        console.log(data.answer);
+        console.log(correctAnswer);
+    });
+});
 
 http.listen(3000, function(){
       console.log('listening on *:3000');
--- player.js
+++ player.js
@@ -1,4 +1,47 @@
+var _ = require('underscore-node');
+
 exports.user = function(name) {
     this.money = 0;
     this.name = name;
 }
+
+exports.show = function(questions) {
+    this.rounds = ['Jeopardy!', 'Double Jeopardy!'];
+    this.questions = questions;
+
+    this.getByRound = function(round) {
+        return _.filter(this.questions, function(q) {
+            return q.round == this.rounds[round];
+        }, this);
+    }
+
+    this.pickCategories = function(round) {
+        return _.sample(_.uniq(_.map(this.getByRound(round), function(q) {
+            return q.category;
+        })), 4);
+    }
+
+    this.getQuestions = function(round) {
+        var cat = this.pickCategories(round);
+        var questions = _.filter(this.getByRound(round), function(q) {
+            return _.contains(cat, q.category);
+        }, this);
+        var qs = {}
+        _.each(cat, function(c) {
+            qs[c] = [];
+        });
+        _.each(questions, function(q) {
+            qs[q.category].push(q);
+        });
+        return qs;
+    }
+
+    this.getQuestion = function(key) {
+        var value = key.split('::')[0];
+        var cat = key.split('::')[1];
+        return _.filter(this.questions, function(q) {
+            return q.category === cat && q.value === value;
+        })[0];
+    }
+}
+
